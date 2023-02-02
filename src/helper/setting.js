60a48554e5787ccf3d491973c4fa23b1b61b2e52
chienminh5298 <chienminh5298@gmail.com>
2023-02-02 16:54:27 -0500

    update

--- src/helper/setting.js
+++ src/helper/setting.js
@@ -1,2 +1,2 @@
-export const server_url = 'http://13.229.146.66:8888';
-export const server_socket = 'http://13.229.146.66:9999';
+export const server_url = 'http://scraping.minhnguyen.website:8888';
+export const server_socket = 'http://scraping.minhnguyen.website:9999';
